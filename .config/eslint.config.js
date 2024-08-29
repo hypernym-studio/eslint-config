cfdacf3aeec8b25b0b431e8ac8cbb3c97ab33d70
Ivo Dolenc <ivodolenc@gmail.com>
2024-08-29 12:57:12 +0200

    chore: update eslint config

--- .config/eslint.config.js
+++ .config/eslint.config.js
@@ -1,14 +1,4 @@
 import { jsConfig, tsConfig, ignoresConfig } from '../src/index.js'
 
 /** @type {import("eslint").Linter.Config[]} */
-export default [
-  jsConfig,
-  tsConfig,
-  ignoresConfig,
-  {
-    files: tsConfig.files,
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-    },
-  },
-]
+export default [jsConfig, tsConfig, ignoresConfig]
