1cac6cbd33104f8d274430f80675f4570648eab8
Ivo Dolenc <ivodolenc@gmail.com>
2024-11-03 13:50:16 +0100

    chore: update dev tool configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-import { jsConfig, tsConfig, ignoresConfig } from '../src/index.js'
+import { jsConfig, tsConfig, ignoresConfig } from './src/index.js'
 
 /** @type {import("eslint").Linter.Config[]} */
 export default [jsConfig, tsConfig, ignoresConfig]
