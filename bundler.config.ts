906479b202a5a5e54e6a273f41406a8387c60d8c
Ivo Dolenc <ivodolenc@gmail.com>
2025-04-15 14:48:19 +0200

    chore: update main bundler config

--- bundler.config.ts
+++ bundler.config.ts
@@ -1,34 +1,46 @@
 import { defineConfig } from '@hypernym/bundler'
 import { dependencies, devDependencies } from './package.json'
 
+const externals = [
+  ...Object.keys(dependencies),
+  ...Object.keys(devDependencies),
+  'eslint/config',
+]
+
 export default defineConfig({
   entries: [
-    { input: './src/index.js' },
-    { dts: './src/types/index.ts' },
+    {
+      input: './src/index.js',
+      externals,
+    },
+    {
+      dts: './src/types/index.ts',
+      output: './dist/index.d.mts',
+      externals,
+    },
     {
       input: './src/svelte/index.js',
-      externals: [
-        ...Object.keys(dependencies),
-        ...Object.keys(devDependencies),
-      ],
+      externals,
     },
-    { dts: './src/types/svelte/index.ts' },
+    {
+      dts: './src/types/svelte/index.ts',
+      output: './dist/svelte/index.d.mts',
+    },
     {
       input: './src/vue/index.js',
-      externals: [
-        ...Object.keys(dependencies),
-        ...Object.keys(devDependencies),
-        'vue-eslint-parser',
-      ],
+      externals,
     },
-    { dts: './src/types/vue/index.ts' },
+    {
+      dts: './src/types/vue/index.ts',
+      output: './dist/vue/index.d.mts',
+    },
     {
       input: './src/react/index.js',
-      externals: [
-        ...Object.keys(dependencies),
-        ...Object.keys(devDependencies),
-      ],
+      externals,
     },
-    { dts: './src/types/react/index.ts' },
+    {
+      dts: './src/types/react/index.ts',
+      output: './dist/react/index.d.mts',
+    },
   ],
 })
