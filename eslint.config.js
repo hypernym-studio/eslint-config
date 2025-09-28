e38323b9ebee7854a44bfba48c17201dba0d0fe0
Ivo Dolenc <ivodolenc@gmail.com>
2025-09-28 16:35:19 +0200

    chore: update dev tools configs

---
+++ eslint.config.js
@@ -0,0 +1,3 @@
+import { defineConfig, jsConfig, tsConfig, ignoresConfig } from './src/index.js'
+
+export default defineConfig([jsConfig, tsConfig, ignoresConfig])
--- eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-import {
-  defineConfig,
-  jsConfig,
-  tsConfig,
-  ignoresConfig,
-} from './src/index.mjs'
-
-export default defineConfig([jsConfig, tsConfig, ignoresConfig])
